3082921d82e87931d759aa062114eb17078e65f5
OttoReuter <detlef@dgebhardt.de>
2026-02-13 13:25:53 +0100

    Update DE keymap for character input handling

--- PBC_PasswordManager/KeyboardDE.cpp
+++ PBC_PasswordManager/KeyboardDE.cpp
@@ -25,44 +25,80 @@ void KeyboardDE::pressTab() {
   Keyboard.write(KEY_TAB);
 }
 
-// ================= DE Keymap =================
+// ======================================================
+//                DE KEYMAP
+// ======================================================
 void KeyboardDE::sendChar(char c) {
+
   switch (c) {
 
+    // =========================
+    // Y / Z Tausch
+    // =========================
+    case 'z': Keyboard.write('y'); return;
+    case 'Z': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('y'); break;
+
+    case 'y': Keyboard.write('z'); return;
+    case 'Y': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('z'); break;
+
+    // =========================
+    // AltGr Kombinationen
+    // =========================
     case '@':
       Keyboard.press(KEY_RIGHT_ALT);
       Keyboard.press('q');
       break;
 
-    case '?':
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(0x2D); // ß
+    case '{':
+      Keyboard.press(KEY_RIGHT_ALT);
+      Keyboard.press('7');
       break;
 
-    case ')':
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press('9');
-      break;
-
-    case '(':
-      Keyboard.press(KEY_LEFT_SHIFT);
+    case '[':
+      Keyboard.press(KEY_RIGHT_ALT);
       Keyboard.press('8');
       break;
 
-    case '%':
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press('5');
+    case ']':
+      Keyboard.press(KEY_RIGHT_ALT);
+      Keyboard.press('9');
       break;
 
-    case '!':
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press('1');
+    case '}':
+      Keyboard.press(KEY_RIGHT_ALT);
+      Keyboard.press('0');
       break;
 
-    case '+':
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press('1'); // ggf. anpassen
+    case '|':
+      Keyboard.press(KEY_RIGHT_ALT);
+      Keyboard.press('<');
       break;
+
+    case '~':
+      Keyboard.press(KEY_RIGHT_ALT);
+      Keyboard.press('+');
+      break;
+
+    // =========================
+    // Shift Sonderzeichen
+    // =========================
+    case '!': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('1'); break;
+    case '"': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('2'); break;
+    case '#': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('3'); break;
+    case '$': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('4'); break;
+    case '%': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('5'); break;
+    case '&': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('6'); break;
+    case '/': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('7'); break;
+    case '(': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('8'); break;
+    case ')': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('9'); break;
+    case '=': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('0'); break;
+    case '?': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('/'); break;
+    case '*': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('+'); break;
+    case '\'': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('#'); break;
+    case ':': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('.'); break;
+    case ';': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press(','); break;
+    case '_': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('-'); break;
+    case '>': Keyboard.press(KEY_LEFT_SHIFT); Keyboard.press('<'); break;
 
     default:
       Keyboard.write(c);
