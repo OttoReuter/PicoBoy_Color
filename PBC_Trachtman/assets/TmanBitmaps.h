00e9576d6efe7b39966ad76a0afa72e2b9fc26e2
OttoReuter <detlef@dgebhardt.de>
2026-03-17 10:34:21 +0100

    Add files via upload

---
+++ PBC_Trachtman/assets/TmanBitmaps.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Arduino.h>  // für uint16_t
+
+// Trachtman Animation Frames
+extern const uint16_t t1left[2048];
+extern const uint16_t t1right[2048];
+extern const uint16_t t2left[2048];
+extern const uint16_t t2right[2048];
+extern const uint16_t t3left[2048];
+extern const uint16_t t3right[2048];
+extern const uint16_t t4left[2048];
+extern const uint16_t t4right[2048];
